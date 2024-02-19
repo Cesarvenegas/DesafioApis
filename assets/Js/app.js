a7a76710f166e5961c7bc8663ef11bec49a7d974
Cesar <cesar.venegas.fmail.com>
2024-02-18 22:59:02 -0300

    grafico

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,31 +1,40 @@
-const btnBuscar = document.getElementById('buscar')
+const btnBuscar = document.getElementById('buscar');
 
-const obtenerInformacion =async()=>{
-try{
-    let resultado = await fetch('https://mindicador.cl/api/')
-    let data = await resultado.json()
-    return data;
-}catch (error){
-    alert('api no disponible')
-}
+const obtenerInformacion = async () => {
+    try {
+        let resultado = await fetch('https://mindicador.cl/api/');
+        let data = await resultado.json();
+        return data;
+    } catch (error) {
+        alert('API no disponible');
+    }
+};
 
-} 
+const imprimirResultado = (data) => {
+    let monto = document.getElementById('monto').value;
+    let moneda = document.getElementById('monedas').value;
+    let valor = data[moneda].valor * parseInt(monto);
+    console.log(data);
+    document.getElementById('resultado').innerHTML = valor;
+};
 
+const ctx = document.getElementById('myChart');
 
-const imprimirResultado =(data)=>{
-    let monto = document.getElementById('monto').value
-    let moneda = document.getElementById('monedas').value
-    let valor = data[moneda].valor * parseInt(monto)
-    console.log(data)
-    document.getElementById('resultado').innerHTML=valor
-}
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    }
+});
 
-
-
-
-btnBuscar.addEventListener('click' ,async(event)=> {
-    event.preventDefault()
-    let data = await obtenerInformacion()
-    imprimirResultado(data)
-})
+btnBuscar.addEventListener('click', async (event) => {
+    event.preventDefault();
+    let data = await obtenerInformacion();
+    imprimirResultado(data);
+});
 
