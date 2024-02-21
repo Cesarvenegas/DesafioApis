1d639caf6a36eecad0f7e5ecc34a9f586f4460f8
Cesar <cesar.venegas.fmail.com>
2024-02-20 21:10:37 -0300

    grafico funcionando

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,40 +1,54 @@
-const btnBuscar = document.getElementById('buscar');
-
-const obtenerInformacion = async () => {
+const btnBuscar = document.getElementById('buscar')
+const ctx = document.getElementById('myChart');
+let monedaSelect = document.getElementById('monedas')
+const obtenerInformacion = async (moneda) => {
     try {
-        let resultado = await fetch('https://mindicador.cl/api/');
+        let resultado = await fetch(`https://mindicador.cl/api/${moneda}`)
         let data = await resultado.json();
-        return data;
+        return data
     } catch (error) {
         alert('API no disponible');
     }
-};
+}
+
+
+const crearGrafico=(fechas,datos)=>{
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: fechas,
+            datasets: [{
+                label: 'Variaciones',
+                data: datos,
+                borderWidth: 1
+            }]
+        }
+    });
+}
 
 const imprimirResultado = (data) => {
     let monto = document.getElementById('monto').value;
-    let moneda = document.getElementById('monedas').value;
-    let valor = data[moneda].valor * parseInt(monto);
+    let moneda = monedaSelect.value
+    let valor = data.serie[0].valor * parseInt(monto)
     console.log(data);
-    document.getElementById('resultado').innerHTML = valor;
+    document.getElementById('resultado').innerHTML = valor
+    let series = data.serie.slice(0,9)
+    let fechas = series.map(item =>{
+        return new Date(item.fecha).toLocaleDateString('en-gb')
+    })
+    let datos = series.map(item =>item.valor)
+    crearGrafico(fechas,datos)
+    console.log(series)
 };
 
-const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-        }]
-    }
-});
 
-btnBuscar.addEventListener('click', async (event) => {
-    event.preventDefault();
-    let data = await obtenerInformacion();
-    imprimirResultado(data);
-});
+
+
+btnBuscar.addEventListener('click', async (event)=>{
+    event.preventDefault()
+    let moneda = monedaSelect.value
+    let data = await obtenerInformacion( moneda)
+    imprimirResultado(data)
+})
 
